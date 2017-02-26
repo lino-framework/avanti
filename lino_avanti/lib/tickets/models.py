b23d0bed5aede4d73f59876d89ffd70db4665837
Luc Saffre <luc.saffre@gmail.com>
2017-02-26 03:52:42 +0200

    http://luc.lino-framework.org/blog/2017/0226.html

--- lino_avanti/lib/tickets/models.py
+++ lino_avanti/lib/tickets/models.py
@@ -25,28 +25,24 @@ class TicketDetail(TicketDetail):
     main = "general #history_tab more"
 
     general = dd.Panel("""
-    general1:60 votes.VotesByVotable:20
-    description:30 comments.CommentsByRFC:30 #clocking.SessionsByTicket:20
-    """, label=_("General"))
-
-    general1 = """
     summary:40 end_user
-    id:6 user:12 faculty
+    id:6 user:12 
     workflow_buttons
-    """
+    description
+    """, label=_("General"))
 
     # history_tab = dd.Panel("""
     # changes.ChangesByMaster:50 #stars.StarsByController:20
     # """, label=_("History"), required_roles=dd.login_required(Triager))
 
     more = dd.Panel("""
-    more1:60 faculties.AssignableWorkersByTicket:20
-    upgrade_notes:20 #LinksByTicket:20
+    more1:60 
+    upgrade_notes:20
     """, label=_("More"), required_roles=dd.login_required(Triager))
 
     more1 = """
     created modified ticket_type:10
-    #topic deadline site 
+    deadline site 
     state priority project
     # standby feedback closed
     """
