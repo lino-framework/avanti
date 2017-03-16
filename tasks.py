e712e700039c3fb3edbc535a80d4d8dba2f1a06e
Luc Saffre <luc.saffre@gmail.com>
2017-03-16 16:31:28 +0200

    http://luc.lino-framework.org/blog/2017/0316.html

--- tasks.py
+++ tasks.py
@@ -2,7 +2,7 @@ from lino.invlib.ns import ns
 ns.setup_from_tasks(
     globals(), "lino_avanti",
     languages="en de fr".split(),
-    # tolerate_sphinx_warnings=True,
+    tolerate_sphinx_warnings=False,
     locale_dir='lino_avanti/lib/avanti/locale',
     revision_control_system='git',
     cleanable_files=['docs/api/lino_avanti.*'],
