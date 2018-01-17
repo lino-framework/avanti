8e15cc2cc31ecec52d315647f7531613089af7ae
Luc Saffre <luc.saffre@gmail.com>
2018-01-17 22:37:24 +0200

    http://luc.lino-framework.org/blog/2018/0117.html

--- lino_avanti/lib/avanti/models.py
+++ lino_avanti/lib/avanti/models.py
@@ -380,7 +380,7 @@ class ClientDetail(dd.DetailLayout):
 
     person = dd.Panel("""
     first_name middle_name last_name #declared_name
-    nationality:15 nationality2:15 birth_country birth_place in_belgium_since 
+    nationality:15 nationality2:15 birth_country birth_place in_belgium_since in_region_since
     card_type #card_number card_issuer card_valid_from card_valid_until
     needs_work_permit has_contact_pcsw has_contact_work_office
     clients.ContactsByClient uploads.UploadsByClient excerpts.ExcerptsByProject:30
